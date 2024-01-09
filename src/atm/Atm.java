9e2e5b00d222581617d64a09a7ddb59f5eaea275
Anastasiya Vasilevskaya <nastena2004vasil@gmail.com>
2024-01-09 23:28:39 +0300

    task 2. atm the recursive method

--- src/atm/Atm.java
+++ src/atm/Atm.java
@@ -9,6 +9,7 @@ package atm;
 //    При снятии денег функция должна распечатывать каким количеством
 //    купюр какого номинала выдаётся сумма.
 //    Создать конструктор с тремя параметрами - количеством купюр каждого номинала.
+
 public class Atm {
     int num20;
     int num50;
@@ -27,29 +28,52 @@ public class Atm {
     }
 
     boolean removeMoney(int value) {
-        int num100 = value / 100;
-        int count100 = (num100 > this.num100) ? this.num100 : num100;
-        value -= count100 * 100;
+        int num20 = this.num20;
+        int num50 = this.num50;
+        int num100 = this.num100;
 
-        int num50 = value / 50;
-        int count50 = (num50 > this.num50) ? this.num50 : num50;
-        value -= count50 * 50;
-
-        int num20 = value / 20;
-        int count20 = (num20 > this.num20) ? this.num20 : num20;
-        value -= count20 * 20;
-
-        if (value == 0) {
-            System.out.println("Купюры номиналом 100: " + count100);
-            System.out.println("Купюры номиналом 50: " + count50);
-            System.out.println("Купюры номиналом 20: " + count20);
+        if (removeMoneyRec(value)) {
+            System.out.println("Выдано купюр номиналом 100: " + (num100 - this.num100));
+            System.out.println("Выдано купюр номиналом 50: " + (num50 - this.num50));
+            System.out.println("Выдано купюр номиналом 20: " + (num20 - this.num20));
             return true;
         } else {
+            this.num20 = num20;
+            this.num50 = num50;
+            this.num100 = num100;
             return false;
         }
-
-
     }
 
+    boolean removeMoneyRec(int value) {
+        if (value == 0) {
+            return true;
+        }
 
+        if (value >= 100 && num100 > 0) {
+            num100--;
+            if (removeMoneyRec(value - 100)) {
+                return true;
+            }
+            num100++;
+        }
+
+        if (value >= 50 && num50 > 0) {
+            num50--;
+            if (removeMoneyRec(value - 50)) {
+                return true;
+            }
+            num50++;
+        }
+
+        if (value >= 20 && num20 > 0) {
+            num20--;
+            if (removeMoneyRec(value - 20)) {
+                return true;
+            }
+            num20++;
+        }
+        return false;
+
+    }
 }
--- src/atm/Main.java
+++ src/atm/Main.java
@@ -2,7 +2,7 @@ package atm;
 
 public class Main {
     public static void main(String[] args) {
-        Atm atm = new Atm (0, 0, 2);
-        System.out.println(atm.removeMoney(20));
+        Atm atm = new Atm(0, 0, 2);
+        System.out.println(atm.removeMoney(150));
     }
 }
