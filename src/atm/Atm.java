1547c35d656d028e64ef680505bea9f1b1159783
Anastasiya Vasilevskaya <nastena2004vasil@gmail.com>
2024-01-09 19:51:36 +0300

    task 2. atm the greedy algorithm

--- src/atm/Atm.java
+++ src/atm/Atm.java
@@ -14,9 +14,42 @@ public class Atm {
     int num50;
     int num100;
 
-    public void addMoney(int num20, int num50, int num100){
+    public Atm(int num20, int num50, int num100) {
+        this.num20 = num20;
+        this.num50 = num50;
+        this.num100 = num100;
+    }
+
+    void addMoney(int num20, int num50, int num100) {
         num20 += num20;
         num50 += num50;
         num100 += num100;
     }
+
+    boolean removeMoney(int value) {
+        int num100 = value / 100;
+        int count100 = (num100 > this.num100) ? this.num100 : num100;
+        value -= count100 * 100;
+
+        int num50 = value / 50;
+        int count50 = (num50 > this.num50) ? this.num50 : num50;
+        value -= count50 * 50;
+
+        int num20 = value / 20;
+        int count20 = (num20 > this.num20) ? this.num20 : num20;
+        value -= count20 * 20;
+
+        if (value == 0) {
+            System.out.println("Купюры номиналом 100: " + count100);
+            System.out.println("Купюры номиналом 50: " + count50);
+            System.out.println("Купюры номиналом 20: " + count20);
+            return true;
+        } else {
+            return false;
+        }
+
+
+    }
+
+
 }
--- src/atm/Main.java
+++ src/atm/Main.java
@@ -2,6 +2,7 @@ package atm;
 
 public class Main {
     public static void main(String[] args) {
-
+        Atm atm = new Atm (0, 0, 2);
+        System.out.println(atm.removeMoney(20));
     }
 }
